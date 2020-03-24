d9729c729b74f87c417aba875d6bbeb1c2b11a4f
ayushjain1144 <ayushjain1144@gmail.com>
2020-03-25 03:12:17 +0530

    activations added

---
+++ activations.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+#sigmoid activation
+
+#sigmoid(x) = 1 / (1 + e^-x)
+def sigmoid(X):
+    return 1.0 / (1.0 + np.exp(-X))
+
+# sigmoid'(x) = sigmoid(x) * (1 - sigmoid(x))
+def sigmoid_grad(X):
+    s = sigmoid(X)
+    return s * (1.0 - s)
+
+#tanh activation
+
+# tanh(x) = e^x - e^-x / e^x + e^-x
+def tanh(X):
+    X_exp = np.exp(X)
+    X_neg_exp = np.exp(-X)
+    return (X_exp - X_neg_exp) / (X_exp + X_neg_exp)
+
+# tanh'(x) = 1 - tanh(x)^2
+def tanh_grad(X):
+    tan = tanh(X)
+    return 1.0 - (tan * tan)
+
+# Relu
+
+# relu(X) = max(0, X)
+
+def relu(X):
+    return np.maximum(0, X)
+
+# relu'(X) = 0 for X <= 0, 1 for X > 0
+
+def relu_grad(X):
+    dX = np.zeros_like(X)
+    dX[X>0] = 1
+    return dX
+
+
+#leaky Relu
+
+# leaky_relu(X) = 1 for X > 0, else 0.1
+def leaky_relu(X, alpha = 0.01):
+    lrelu = np.copy(X)
+    lrelu[X<=0] *= alpha
+    return lrelu
+
+#leaky_relu'(X) = 1 for X > 0, else alpha
+def leaky_relu_grad(X, alpha):
+    dX = np.ones_like(X)
+    dX[X<=0] = alpha
+    return dX
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
