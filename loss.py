e0c2c9bfd5523ada9926cf3e1d37c1806841ad30
ayush jain <ayushjain1144@gmail.com>
2020-06-25 09:06:33 +0530

    Fixed bugs

--- loss.py
+++ loss.py
@@ -3,8 +3,8 @@ import numpy as np
 
 #stable softmax(X) = exp(Xi + logc)/(sigma(Xij + logC))
 def softmax(X):
-    exps = np.exp(X - np.max(X))
-    return exps / np.sum(exps) 
+    exps = np.exp(X - np.max(X, axis=0))
+    return exps / np.sum(exps, axis=0) 
 
 # loss = sigma(y * log (softmax(X)))
 def cross_entropy_loss(X, y, lamda = 0):
